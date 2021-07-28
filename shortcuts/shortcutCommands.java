beed40293a3503f28339d55e7e85bb96928b20b7
Wasin Sanguansuk <wasin_sanguansuk@hotmail.com>
2021-07-28 17:17:32 -0500

    Initial Commit

---
+++ shortcuts/shortcutCommands.java
@@ -0,0 +1,69 @@
+package shortcuts;
+
+public class shortcutCommands {
+
+    public static void main(String[] args) {
+
+        System.out.println(" ---------- ---------- ---------- ---------- ---------- ");
+        System.out.println();
+
+        System.out.println("[_ Shortcut Commands - IntelliJ _]");
+
+        System.out.println("To accept the autocomplete, press Enter or Tab.");
+        System.out.println("To ignore the autocomplete, press Space.");
+
+        System.out.println("Alt+1			---> Open/Close the Project window");
+
+        System.out.println("Alt+Insert 		---> Create constructor, toString(), ...");
+        System.out.println("Ctrl+/			---> Line comment");
+        System.out.println("Ctrl+Shift+/	---> Block comment");
+
+        System.out.println("Ctrl+Alt+L 		---> Re-format codes");
+
+        System.out.println("Ctrl+Shift+F10	---> Run (First run)");
+        System.out.println("Shift+F10	    ---> Re-Run");
+        System.out.println("Shift+F9	    ---> Debug");
+
+        System.out.println("F9              ---> Debug: Resume program (Important)");
+        System.out.println("F8              ---> Debug: Step over");
+        System.out.println("F7              ---> Debug: Step into");
+        System.out.println("Alt+Shift+F7    ---> Debug: (Force) Step into");
+        System.out.println("Shift+F8        ---> Debug: Step out");
+        System.out.println("Alt+F9          ---> Debug: Run to cursor");
+
+        System.out.println("Alt+F7			---> Find all usages");
+        System.out.println("Ctrl+B 			---> Goto declaration and usage");
+        System.out.println("Ctrl+Alt+B 		---> Goto implementation");
+
+        System.out.println("Ctrl+Shift+F    ---> Find");
+
+        System.out.println();
+        System.out.println(" ---------- ---------- ---------- ---------- ---------- ");
+        System.out.println();
+
+        System.out.println("[_ Shortcut Commands - Eclipse _]");
+
+        System.out.println("To accept the autocomplete, press Enter or Tab, Enter.");
+        System.out.println("To ignore the autocomplete, press Space.");
+
+        System.out.println("			    ---> Open/Close the Project window");
+
+        System.out.println(" 		        ---> Create constructor, toString(), ...");
+        System.out.println("Ctrl+/			---> Line comment");
+        System.out.println("	            ---> Block comment");
+
+        System.out.println("Ctrl+Alt+F 		---> Auto format");
+
+        System.out.println("Ctrl+F11	    ---> Run");
+
+        System.out.println("			    ---> Find all usages");
+        System.out.println("			    ---> Goto declaration and usage");
+        System.out.println("	        	---> Goto implementation");
+
+        System.out.println();
+        System.out.println("---------- ---------- ---------- ---------- ---------- ");
+
+        
+
+    }
+}
