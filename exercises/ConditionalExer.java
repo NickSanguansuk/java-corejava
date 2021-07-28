47b6164841b31002372851d27814c856791e7434
Wasin Sanguansuk <wasin_sanguansuk@hotmail.com>
2021-07-28 18:09:19 -0500

    Working on Conditional Exercise

--- exercises/ConditionalExer.java
+++ exercises/ConditionalExer.java
@@ -1,4 +1,74 @@
 package exercises;
 
 public class ConditionalExer {
+    // Core Java - Exercise - Control Flow (Conditional) Statements
+
+    static void problem1() {
+        System.out.println("Problem 1: ");
+        //int x = 7;
+        int x = 15;
+
+        if (x < 10) {
+            System.out.println("Less than 10");
+        }
+    }
+
+    static void problem2() {
+        System.out.println("Problem 2: ");
+        //int x = 7;
+        int x = 15;
+
+        if (x < 10) {
+            System.out.println("Less than 10");
+        } else {
+            System.out.println("Greater than 10");
+        }
+    }
+
+    static void problem3() {
+        System.out.println("Problem 3: ");
+        //int x = 15;
+        int x = 50;
+
+        if (x < 10) {
+            System.out.println("Less than 10");
+        } if (x < 20) {
+            System.out.println("Between 10 and 20");
+        } else {
+            System.out.println("Greater than or equal 20");
+        }
+    }
+
+    static void problem4() {
+        System.out.println("Problem 4: ");
+
+
+    }
+
+    static void problem5() {
+        System.out.println("Problem 5: ");
+
+
+    }
+
+    static void problem6() {
+        System.out.println("Problem 6: ");
+
+
+    }
+    public static void main(String[] args) {
+
+        problem1();
+        System.out.println(" ---------- ");
+        problem2();
+        System.out.println(" ---------- ");
+        problem3();
+        System.out.println(" ---------- ");
+        problem4();
+        System.out.println(" ---------- ");
+        problem5();
+        System.out.println(" ---------- ");
+        problem6();
+        System.out.println(" ---------- ");
+    }
 }
--- topics/BasicsDemo.java
+++ topics/BasicsDemo.java
@@ -3,7 +3,7 @@ package topics;
 public class BasicsDemo {
     // Core Java - Introduction to Java Basics
     // Core Java - Java Programming Fundamentals
-    
+
     public static void main(String[] args) {
 
     }
