47ad8e5997c49e4f24b1f6c464c283ec7553dd83
Wasin Sanguansuk <wasin_sanguansuk@hotmail.com>
2022-02-21 10:59:09 -0600

    Added Files for BFS training

---
+++ exercises/training_bfs/week1/day1/HelloBFS.java
@@ -0,0 +1,8 @@
+package exercises.training_bfs.week1.day1;
+
+public class HelloBFS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello BFS February Java Training Batch.");
+    }
+}
