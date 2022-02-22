b0b4c5de5da1c00d9d9b80cc7fa403e3ea6c8663
Wasin Sanguansuk <wasin_sanguansuk@hotmail.com>
2022-02-22 01:47:31 -0600

    Added exercise Week1Day1 BFS

---
+++ exercises/training_bfs/week1/day1/HW_HelloWorld.java
@@ -0,0 +1,9 @@
+package exercises.training_bfs.week1.day1;
+
+public class HW_HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
---
+++ exercises/training_bfs/week1/day1/HW_ReverseString.java
@@ -0,0 +1,37 @@
+package exercises.training_bfs.week1.day1;
+
+public class HW_ReverseString {
+
+    String ReverseString(String strIn) {
+
+        String strOut = "";
+
+        int len = strIn.length();
+        for (int i = 0; i < len; i++) {
+            strOut += strIn.charAt(len - i - 1);
+        }
+
+        return strOut;
+    }
+
+    String ReverseString2(String strIn) {
+
+        String strOut = "";
+
+        int len = strIn.length();
+        for (int i = 0; i < len; i++) {
+            strOut = strIn.charAt(i) + strOut;
+        }
+
+        return strOut;
+    }
+
+    public static void main(String[] args) {
+
+        HW_ReverseString obj = new HW_ReverseString();
+        String inputStr = "Nick (Wasin) Sanguansuk";
+        System.out.println(obj.ReverseString(inputStr));
+        System.out.println(obj.ReverseString2(inputStr));
+    }
+}
+
