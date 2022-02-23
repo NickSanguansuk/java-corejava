a6eafdf95a88dd23e2533824e5b2285129e52ef2
Wasin Sanguansuk <wasin_sanguansuk@hotmail.com>
2022-02-23 00:55:48 -0600

    Added BFS week 1 day 3 Demo

---
+++ exercises/training_bfs/week1/day3/Demo01.java
@@ -0,0 +1,18 @@
+package exercises.training_bfs.week1.day3;
+
+public class Demo01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("---------- ---------- ---------- ---------- ----------");
+        {
+
+        }
+        System.out.println("---------- ---------- ---------- ---------- ----------");
+        {
+
+        }
+        System.out.println("---------- ---------- ---------- ---------- ----------");
+
+    }
+}
